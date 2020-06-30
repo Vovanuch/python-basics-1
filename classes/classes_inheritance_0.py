a355499007bdba67ee899db39a6e563469d8e926
Vladimir <vetohin.vladimir@gmail.com>
2020-06-30 07:46:30 +0300

    2020.06.30, few new scripts

--- classes/classes_inheritance_0.py
+++
@@ -1,6 +0,0 @@
-''' Классы. Наследование (inheritance). '''
-
-class DerivedClassName(Base1, Base2, Base3): #Base1, Base2, Base3 - имена классов, от которых будет наследовать наш новый класс  DerivedClassName
-    <statement 1>
-    ...
-    <statement n>
